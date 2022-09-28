54baac36c1da56744728af11eb59bec81cd6a4ca
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-09-28 20:39:41 -0300

    js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 console.log("hola");
 
-let img = document.getElementsByClassName("Imagenes")
-let idImage = document.querySelectorAll("#ImagFig")
-let cajaImgChicas = document.querySelector(".cajaImgChicas")
-let cajaGrande = document.querySelector(".cajaGrande")
-let cajaImgGrande = document.querySelector(".cajaImgGrande")
-let imgGrande =document.getElementById("imgGrande")
+let img = document.getElementsByClassName("Imagenes")// clase de cada imagen en en cajaImgChicas
+let idImage = document.querySelectorAll("#ImagFig") //id de figure 
+let cajaImgChicas = document.querySelector(".cajaImgChicas") //box-imagenes
+let cajaGrande = document.querySelector(".cajaGrande")//contentImagenes
+let cajaImgGrande = document.querySelector(".cajaImgGrande")//boxLargeImagen
+let imgGrande =document.getElementById("imgGrande")//boxImagen
 let body = document.querySelector(".body")
 let close = document.querySelector(".Close")
 
@@ -15,13 +15,13 @@ for( const idimagen of idImage){
     idimagen.addEventListener("click", (envet) => {
         cajaImgChicas.classList.add('resize-box-imagenes');
         cajaGrande.classList.add('content-image-resize');
-        cajaImgGrande.classList.add('block.imagen');
-        body.classList.add('resize-body');
+        cajaImgGrande.classList.add('block-imagen');
+       body.classList.add('resize-body');
     })
 }
 
 let n = 0;
 function seleccionar(n){
     let obj = img.item(n);
-    cajaImgChicas.src = obj.src
+    cajaImgChicas.src = obj.src;
 }
